6b90f7e751e47901531aba6d0b0e3043d4aec5a8
Assylkhan81 <asylkhan@list.ru>
2022-08-28 16:59:48 +0600

    Лекция 1 в примерах

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿// не понял в данной программе ни условия ни решения
+// Console.Clear();
+// Console.SetCursorPosition(10, 4);
+// Console.WriteLine("+");
+int xa = 1;
+int ya = 1;
+int xb = 1;
+int yb = 30;
+int xc = 80;
+int yc = 30;
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+
+//Задача с гирями
+    // int a = 1;
+    // int b = 2;
+    // int c = 6;
+    // int d = 8;
+    // int e = 4;
+    // int max = a;
+    // if (a > max ) max = a;
+    // if (b > max ) max = b;
+    // if (c > max ) max = c;
+    // if (d > max ) max = d;
+    // if (e > max ) max = e;
+    // Console.Write("max = " + max);
+
+
+//Оператор условия if
+    // Console.Write("Введите имя пользователя: ");
+    // string username = Console.ReadLine();
+    // if (username == "Маша")
+    // {
+    //     Console.WriteLine("Ура, это же МАША!!!");
+    // }
+    // else
+    // {
+    //     Console.Write("Привет, ");
+    //     Console.WriteLine(username);
+    // }
+    
+
+// Приветствие
+    // Console.WriteLine("Введите Ваше имя ");
+    // string username = Console.ReadLine();
+    // Console.WriteLine("Привет, ");
+    // Console.WriteLine(username);
+
+// сумма двух чисел
+    // Console.WriteLine("Введите 1-ое число ");
+    // int numberA = Convert.ToInt32(Console.ReadLine());
+    // Console.WriteLine("Введите 2-ое число ");
+    // int numberВ = Convert.ToInt32(Console.ReadLine());
+    // int result = numberA + numberВ;
+    // Console.WriteLine(result);
+
+// частное двух чисел
+    // Console.WriteLine("Введите 1-ое число ");
+    // double numberA = Convert.ToInt32(Console.ReadLine());
+    // Console.WriteLine("Введите 2-ое число ");
+    // double numberB = Convert.ToInt32(Console.ReadLine());
+    // Console.WriteLine($"частное чисел {numberA} и {numberB} равно " + numberA / numberB);
+
+// Рандом -случайные числа
+    // int numberA = new Random().Next(1000,99999);
+    // Console.WriteLine("Первое случайное число " + numberA);
+    // int numberB = new Random().Next(1,10);
+    // Console.WriteLine("Второе случайное число " + numberB);
+    // int result = numberA + numberB;
+    // Console.WriteLine($"Сумма случайных чисел: {numberA} + {numberB} = " + result);
+
+
